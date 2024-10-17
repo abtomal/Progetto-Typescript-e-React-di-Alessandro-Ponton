3f1aae21eb8ca136dcfc77fd84481eb574fbc058
abtomal <abtomalmusic@gmail.com>
2024-10-17 14:29:43 +0200

    ssssss

--- Progetto Typescript e React di Alessandro Ponton/src/main.tsx
+++ Progetto Typescript e React di Alessandro Ponton/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style.css';
+import './styles.css';
 
 
 const rootElement = document.getElementById('app');
