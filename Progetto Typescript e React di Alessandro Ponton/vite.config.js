e53c370013b5cc1776dcfe9d819c8bc644f54354
abtomal <abtomalmusic@gmail.com>
2024-10-17 14:42:03 +0200

    yy

--- Progetto Typescript e React di Alessandro Ponton/vite.config.js
+++ Progetto Typescript e React di Alessandro Ponton/vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
+          // Divide dependencies into separate chunks for better caching
           vendor: ['react', 'react-dom', 'react-router-dom'],
         },
       },
     },
   },
   server: {
+    // Fallback to index.html for SPA routing
     historyApiFallback: true,
   },
 });
