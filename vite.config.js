74dc7cf0438af1b13adba2a092a23b049276049c
abtomal <abtomalmusic@gmail.com>
2024-10-20 11:27:34 +0200

    xxx

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Desktop\Progetto Typescript e React di Alessandro Ponton/',
+  base: '/quizfashion/',  
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
   server: {
-    host: 'localhost', 
-    port: 5173,        
-    strictPort: true, 
-    open: true         
+    host: 'localhost',
+    port: 5173,
+    strictPort: true,
   }
 });
